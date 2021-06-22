fc60ebd14af99f398ee80ac2004b7b8662af7d79
KaidenGroves <85904637+Kaiden-Groves@users.noreply.github.com>
2021-06-22 20:43:37 +0000

    Version 1.0

--- script.js
+++ script.js
@@ -4,18 +4,47 @@
 //let canvasDiv;
 
 // Global ML Variables
+let mobilenet;
+
+let video;
+
+let isModelReady;
 //let mobilenet;
 
 function setup() {
+  canvasDiv = createDiv();
+
+  canvas = createCanvas(640, 480);
+
+  canvas.parent(canvasDiv);
+
+  textDiv = createDiv();
+
+  textP = createP("Model loading, please wait...");
+
+  textP.parent(textDiv);
+
+  video = createCapture(VIDEO);
+
+  video.style("display", "none");
+
+  
+
 
 }
 
 function draw() {
+  image(video, 0, 0);
 
+  if(isModelReady) {
+
+  mobilenet.classify(canvas, gotResults);
+
+    }
 }
 
 function modelReady() {
-
+  isModelReady = true;
 }
 
 function gotResults(error, results) {
